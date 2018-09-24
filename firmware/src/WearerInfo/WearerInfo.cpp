0680ce0fa21bbccde127a315c2b6c5644669a63c
Brandon Satrom <bsatrom@gmail.com>
2018-09-24 15:31:41 -0500

    put EEPROM code back in place; closes #149

--- firmware/src/WearerInfo/WearerInfo.cpp
+++ firmware/src/WearerInfo/WearerInfo.cpp
@@ -2,7 +2,7 @@
 
 WearerInfo::WearerInfo()
 {
-  // EEPROM.get(EEPROM_SET_LOCATION, _isSet);
+  EEPROM.get(EEPROM_SET_LOCATION, _isSet);
 
   if (_isSet == 1)
   {
@@ -21,8 +21,8 @@ void WearerInfo::setFirstName(String name)
   _firstName = name;
   _isSet = 1;
 
-  // EEPROM.put(EEPROM_FIRSTNAME_LOCATION, stringBuff);
-  // EEPROM.put(EEPROM_SET_LOCATION, _isSet);
+  EEPROM.put(EEPROM_FIRSTNAME_LOCATION, stringBuff);
+  EEPROM.put(EEPROM_SET_LOCATION, _isSet);
 }
 
 void WearerInfo::setLastName(String name)
@@ -34,8 +34,8 @@ void WearerInfo::setLastName(String name)
   _lastName = name;
   _isSet = 1;
 
-  // EEPROM.put(EEPROM_LASTNAME_LOCATION, stringBuff);
-  // EEPROM.put(EEPROM_SET_LOCATION, _isSet);
+  EEPROM.put(EEPROM_LASTNAME_LOCATION, stringBuff);
+  EEPROM.put(EEPROM_SET_LOCATION, _isSet);
 }
 
 void WearerInfo::setTwitter(String name)
@@ -47,8 +47,8 @@ void WearerInfo::setTwitter(String name)
   _twitter = name;
   _isSet = 1;
 
-  // EEPROM.put(EEPROM_TWITTER_LOCATION, stringBuff);
-  // EEPROM.put(EEPROM_SET_LOCATION, _isSet);
+  EEPROM.put(EEPROM_TWITTER_LOCATION, stringBuff);
+  EEPROM.put(EEPROM_SET_LOCATION, _isSet);
 }
 
 boolean WearerInfo::isSet()
@@ -90,7 +90,7 @@ String WearerInfo::getFromEEPROM(int address)
 {
   char value[STRING_BUFFER_SIZE];
 
-  // EEPROM.get(address, value);
+  EEPROM.get(address, value);
   value[sizeof(value) - 1] = 0;
 
   if (value[0] != 255)
