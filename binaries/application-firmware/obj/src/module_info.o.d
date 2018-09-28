273a8c7dea8ba2c363b629fb19cba64c2c0e52a8
Brandon Satrom <bsatrom@gmail.com>
2018-09-28 17:35:23 -0500

    added modular binaries

---
+++ binaries/application-firmware/obj/src/module_info.o.d
@@ -0,0 +1,33 @@
+../../parti-badge/firmware/target/obj/./src/module_info.o: \
+ src/module_info.c ../dynalib/inc/module_info.inc \
+ ../dynalib/inc/module_info.h ../services/inc/static_assert.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/sys/_stdint.h
+
+../dynalib/inc/module_info.inc:
+
+../dynalib/inc/module_info.h:
+
+../services/inc/static_assert.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/lib/gcc/arm-none-eabi/5.3.1/include/stddef.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/lib/gcc/arm-none-eabi/5.3.1/include/stdint.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/stdint.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/sys/features.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/Cellar/gcc.arm-none-eabi-53/20160307/arm-none-eabi/include/sys/_stdint.h:
